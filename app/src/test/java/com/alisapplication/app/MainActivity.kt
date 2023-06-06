3e8e680b400b729371f1a5f2001d036ead7c52dc
chaht72 <chaht@zainmotors.com.pk>
2023-06-06 04:52:52 +0000

    Code Synced by DhiWise

---
+++ app/src/test/java/com/alisapplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.alisapplication.app
+
+import com.alisapplication.app.appcomponents.base.BaseActivity
+import com.alisapplication.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
